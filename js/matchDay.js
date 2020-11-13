50e06614c73b6632349245e86cd80d1098c15b38
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-13 16:15:45 +0200

    matchDay

---
+++ js/matchDay.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const matchDay = {
+    data: '2020.11.21',
+    day: 'Saturday',
+    time: '21:45',
+    stadium: {
+        name: 'Anfield',
+        address: ['Anfield Road', 'Liverpool', 'L4 0TH'],
+        capacity: '54,074',
+    },
+    teamHome: {
+        name: 'Liverpool',
+        startXI: [
+            alisson becker
+        ]
+        
+    }
+    teamGuest: 'ManchesterCity',
+}
