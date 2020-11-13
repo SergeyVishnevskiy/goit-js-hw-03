0d06373a8f66fd36f080bcfebe3fb27cb12c1dfd
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-13 16:16:20 +0200

    task-02 done

---
+++ js/task-02.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const countProps = function(obj) {
+    'use strict';
+  // Write code under this line
+  const key = Object.keys(obj);
+  return key.length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
