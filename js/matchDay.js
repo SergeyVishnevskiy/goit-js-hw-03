b8e3234a868006faef7c4c583ee6993750db9883
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-14 17:40:06 +0200

    mathDay

--- js/matchDay.js
+++ js/matchDay.js
@@ -11,10 +11,44 @@ const matchDay = {
     },
     teamHome: {
         name: 'Liverpool',
-        startXI: [
-            alisson becker
-        ]
-        
+        startXI: {
+            gk: 'Alisson Becker', 
+            rb: 'Trent Alexander-Arnold',
+            rcb: 'Joel Matip',
+            lcb: 'Virgil (van Dijk)',
+            lb: 'Andrew Robertson',
+            rcm: 'Jordan Henderson (C)',
+            cm: 'Fabinho',
+            lcm: 'Tiago Alcantara',
+            rw : 'Mohamed Salah',
+            st: 'Roberto Firmino',
+            lw: 'Sadio Mane'
+        }, 
+        spareBench: {
+            gk: ['Adrian', 'Caoimhin Kelleher'],
+            def: ['Nathaniel Phillips', 'Konstantinos Tsimikas', 'Neco Williams', 'Sepp van den Berg'],
+            midl: ['Georginio Wijnaldum', 'Naby Keïta', 'Alex Oxlade-Chamberlain', 'James Milner', 'Curtis Jones'],
+            atc: ['Diogo Jota', 'Takumi Minamino', 'Xherdan Shaqiri', 'Divock Origi'],
+
+        }, 
+        coach:{
+            name: 'Jürgen Klopp',
+            nationality: 'Germany',
+            age: 53,
+            trains: '5 years',
+            in_a_position_with: '2015.10.08',
+            contract_to: '2024.06.30',
+        }
+    },
+    teamGuest: {
+        name: 'ManchesterCity',
+    },
+    teamOfArbitrators: {
+        first: 'Michael Oliver',
+        second: 'Stuart Burt',
+        third: 'Simon Bennett',
+        fourth: 'Martin Atkinson',
+        fifth: 'Andrea Marriner',
     }
-    teamGuest: 'ManchesterCity',
 }
+console.log(matchDay);
