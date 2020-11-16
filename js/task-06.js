a847b7fc241114d266ac1b2346555bd18c370d43
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-16 11:01:17 +0200

    task-06

--- js/task-06.js
+++ js/task-06.js
@@ -13,7 +13,7 @@ const products = [
 
 const calculateTotalPrice = function(allProdcuts, productName) {
     let total = 0;
-    console.log(allProdcuts);
+    // console.log(allProdcuts);
     for (const allProdcuts of products) {
         
         if (allProdcuts.name === productName) {
